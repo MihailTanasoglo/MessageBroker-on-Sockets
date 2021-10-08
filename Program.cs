0237255a489a160df20d7fab44137e39e6b3e9ec
MihailTanasoglo <heterszeus@gmail.com>
2021-10-08 08:52:29 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Common;
+using Newtonsoft.Json;
+using System;
+using System.Text;
+
+namespace MessageBroker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("ThisIsSender");
+
+            var senderSocket = new SenderSocket();
+            senderSocket.Connection(Settings.BrokerIp, Settings.BrokerPort);
+
+            if (senderSocket.fConnection)
+            {
+                for (; ;)
+                {
+                    var payload = new PayLoad();
+
+                    Console.Write("Введите тему: ");
+                    payload.Topic = Console.ReadLine().ToLower();
+
+                    Console.Write("Введите текст: ");
+                    payload.Message = Console.ReadLine();
+
+                    var payloadString = JsonConvert.SerializeObject(payload);
+                    byte[] data = Encoding.UTF8.GetBytes(payloadString);
+
+                    senderSocket.Send(data);
+                }
+            }
+                 
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ SenderSocket.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace MessageBroker
+{
+    class SenderSocket
+    {
+        private Socket _socket;
+        public bool fConnection;
+
+        public SenderSocket()
+        {
+            _socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+        }
+        public void Connection(string ipAddress, int port)
+        {
+            //Асинхронный запуск с ожиданием callback
+            _socket.BeginConnect(new IPEndPoint(IPAddress.Parse(ipAddress), port), ConnectedCallback, null);
+            Thread.Sleep(3000);
+        }
+
+        public void Send(byte[] data)
+        {
+            try
+            {
+                _socket.Send(data);
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine($" Couldn`t send data.{ e.Message}");
+            }
+
+        }
+
+
+
+        private void ConnectedCallback(IAsyncResult asyncResult)
+        {
+            if (_socket.Connected)
+            {
+                Console.WriteLine("Отправитель подключился к брокеру сообщений.");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось подключить отправителя к брокеру сообщений.");
+                Console.WriteLine("Код ошибки: Stupid STUDENT");
+            }
+
+            fConnection = _socket.Connected;
+        }
+
+    }
+}
